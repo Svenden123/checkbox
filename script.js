dc86be03f2771d6471811306907fcf3aba7616ce
Svenden123 <buhrus1@yandex.ru>
2021-01-03 15:40:48 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function fun1() {
+    var chbox;
+
+    chbox = document.getElementById('one')
+    if (chbox.checked) {
+        alert('Выбран');
+    } else {
+        alert('Не выбран')
+    }
+}
